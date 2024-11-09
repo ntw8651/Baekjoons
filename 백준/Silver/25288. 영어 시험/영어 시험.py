d321f70829bedee22547fae321355540a406c001
ntw <60605154+ntw8651@users.noreply.github.com>
2024-11-09 18:01:23 +0900

    [Silver IV] Title: 영어 시험, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25288. 영어 시험/영어 시험.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = input()
+length = len(arr)
+for _ in range(n):
+    for i in range(length):
+        print(arr[i], end="")
+
+'''
+최장공통부분 수열을 모두 포함하는...
+
+그러니까 그냥 섞지 말고..?
+abcabcabc
+'''
